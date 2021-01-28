0f416f0ce73c09de0342fbb106d004ae1c1e7cd0
shahnaaz <66459095+shahnaaz20@users.noreply.github.com>
2021-01-28 10:40:01 +0530

    Update codechef.py

--- codechef.py
+++ codechef.py
@@ -1,10 +1,10 @@
-# list = ["a",1,"2",5,"b","q"]
-# n = int(input("a num:"))
-# lenght = len(list)
-# index = lenght - n
-# while index < len(list):
-#     print(list[index])
-#     index = index + 1
+list = ["a",1,"2",5,"b","q"]
+n = int(input("a num:"))
+lenght = len(list)
+index = lenght - n
+while index < len(list):
+    print(list[index])
+    index = index + 1
 
 
 
